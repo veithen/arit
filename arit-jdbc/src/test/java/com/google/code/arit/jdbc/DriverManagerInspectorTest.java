3b269cbfbf0b66b0c73c1692e8f13aba9fb05a7f
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-03-08 22:03:40 +0000

    Use a dummy driver instead of Derby in the arit-jdbc unit tests. This also avoids creation of the derby.log file.

--- arit-jdbc/src/test/java/com/google/code/arit/jdbc/DriverManagerInspectorTest.java
+++ arit-jdbc/src/test/java/com/google/code/arit/jdbc/DriverManagerInspectorTest.java
@@ -1,23 +1,23 @@
 package com.google.code.arit.jdbc;
 
+import java.sql.DriverManager;
 import java.util.List;
 
-import org.apache.derby.jdbc.AutoloadedDriver;
-import org.apache.derby.jdbc.EmbeddedDriver;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.code.arit.ProviderFinder;
-import com.google.code.arit.jdbc.DriverManagerInspector;
 
 public class DriverManagerInspectorTest {
     @Test
-    public void test() {
+    public void test() throws Exception {
         List<DriverManagerInspector> inspectors = ProviderFinder.find(DriverManagerInspector.class);
         Assert.assertEquals(1, inspectors.size());
         DriverManagerInspector inspector = inspectors.get(0);
-        new EmbeddedDriver();
+        MyDriver driver = new MyDriver();
+        DriverManager.registerDriver(driver);
         List<Class<?>> classes = inspector.getDriverClasses();
-        Assert.assertTrue(classes.contains(AutoloadedDriver.class));
+        Assert.assertTrue(classes.contains(MyDriver.class));
+        DriverManager.deregisterDriver(driver);
     }
 }
