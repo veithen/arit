8448b8de0f7e03d6165ef18d969d90b31c4c6342
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-03-08 21:23:57 +0000

    Properly extract the context root from WebappClassLoader (Tomcat).

--- arit-tomcat/src/main/java/com/google/code/arit/tomcat/TomcatProfile.java
+++ arit-tomcat/src/main/java/com/google/code/arit/tomcat/TomcatProfile.java
@@ -1,6 +1,7 @@
 package com.google.code.arit.tomcat;
 
 import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.naming.resources.ProxyDirContext;
 
 import com.google.code.arit.ServerProfile;
 
@@ -8,8 +9,7 @@ public class TomcatProfile implements ServerProfile {
     public String identifyApplication(ClassLoader classLoader) {
         if (classLoader instanceof WebappClassLoader) {
             WebappClassLoader wacl = (WebappClassLoader)classLoader;
-            // TODO: find a way to get the application name
-            return wacl.toString();
+            return ((ProxyDirContext)wacl.getResources()).getContextName();
         } else {
             return null;
         }
