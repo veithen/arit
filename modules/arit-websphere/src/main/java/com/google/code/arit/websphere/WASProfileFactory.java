fc4936efd9ead5a636ab1b5cdff2de0e921b5659
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-03-28 09:44:56 +0000

    Some initial WebSphere JMX code.

--- modules/arit-websphere/src/main/java/com/google/code/arit/websphere/WASProfileFactory.java
+++ modules/arit-websphere/src/main/java/com/google/code/arit/websphere/WASProfileFactory.java
@@ -1,6 +1,12 @@
 package com.google.code.arit.websphere;
 
 import java.lang.reflect.Field;
+import java.util.Set;
+
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
 
 import com.google.code.arit.ServerContext;
 import com.google.code.arit.ServerProfile;
@@ -9,6 +15,24 @@ import com.google.code.arit.util.ReflectionUtil;
 
 public class WASProfileFactory implements ServerProfileFactory {
     public ServerProfile createServerProfile(ServerContext serverContext) {
+        try {
+            serverContext.getApplicationClassLoader().loadClass("com.ibm.websphere.management.AdminServiceFactory");
+        } catch (ClassNotFoundException ex) {
+            return null;
+        }
+        MBeanServer mbeanServer = WASUtil.getMBeanServer();
+        try {
+            Set<ObjectInstance> mbeans = mbeanServer.queryMBeans(new ObjectName("WebSphere:type=Server,*"), null);
+            if (mbeans.size() != 1) {
+                return null;
+            }
+            ObjectInstance server = mbeans.iterator().next();
+            System.out.println(server.getObjectName());
+            System.out.println(server.getClass());
+        } catch (MalformedObjectNameException ex) {
+            return null;
+        }
+        
         Class<?> classLoaderClass = serverContext.getApplicationClassLoader().getClass();
         if (classLoaderClass.getName().equals("com.ibm.ws.classloader.CompoundClassLoader")) {
             Field nameField;
