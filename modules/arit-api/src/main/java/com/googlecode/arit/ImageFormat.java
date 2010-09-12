de40d358288eac56175dace40b4c58669e5a537c
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-09-12 20:45:03 +0000

    Initial code for module types.

---
+++ modules/arit-api/src/main/java/com/googlecode/arit/ImageFormat.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit;
+
+public enum ImageFormat {
+    GIF("image/gif", "gif");
+    
+    private final String contentType;
+    private final String suffix;
+
+    private ImageFormat(String contentType, String suffix) {
+        this.contentType = contentType;
+        this.suffix = suffix;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+}
