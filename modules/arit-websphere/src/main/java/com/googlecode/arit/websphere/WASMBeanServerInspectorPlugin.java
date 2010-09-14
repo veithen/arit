eaab0e77ed1676e062eb4558560a27da83c87538
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-09-14 13:51:51 +0000

    Some more WebSphere code.

---
+++ modules/arit-websphere/src/main/java/com/googlecode/arit/websphere/WASMBeanServerInspectorPlugin.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.websphere;
+
+import javax.management.MBeanServer;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+
+import com.googlecode.arit.mbeans.MBeanRepository;
+import com.googlecode.arit.mbeans.MBeanServerInspector;
+import com.googlecode.arit.mbeans.MBeanServerInspectorPlugin;
+import com.googlecode.arit.rbeans.RBeanFactory;
+import com.googlecode.arit.rbeans.RBeanFactoryException;
+
+@Component(role=MBeanServerInspectorPlugin.class, hint="websphere")
+public class WASMBeanServerInspectorPlugin implements MBeanServerInspectorPlugin {
+    private final RBeanFactory rbf;
+    
+    @Requirement
+    private MBeanServerInspector mbsInspector;
+    
+    public WASMBeanServerInspectorPlugin() {
+        RBeanFactory rbf;
+        try {
+            rbf = new RBeanFactory(PlatformMBeanServerRBean.class);
+        } catch (RBeanFactoryException ex) {
+            rbf = null;
+        }
+        this.rbf = rbf;
+    }
+    
+    public boolean isAvailable() {
+        return rbf != null;
+    }
+
+    public MBeanRepository inspect(MBeanServer mbs) {
+        if (rbf.getRBeanInfo(PlatformMBeanServerRBean.class).getTargetClass().isInstance(mbs)) {
+            return mbsInspector.inspect(rbf.createRBean(PlatformMBeanServerRBean.class, mbs).getDefaultMBeanServer());
+        } else {
+            return null;
+        }
+    }
+}
---
+++ modules/arit-websphere/src/main/java/com/googlecode/arit/websphere/WASModuleListerPlugin.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2010 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.websphere;
+
+import java.util.List;
+
+import javax.management.MBeanServer;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.Logger;
+
+import com.googlecode.arit.ModuleDescription;
+import com.googlecode.arit.ModuleListerPlugin;
+import com.googlecode.arit.mbeans.MBeanRepository;
+import com.googlecode.arit.mbeans.MBeanServerInspector;
+import com.googlecode.arit.rbeans.RBeanFactory;
+import com.googlecode.arit.rbeans.RBeanFactoryException;
+
+@Component(role=ModuleListerPlugin.class, hint="websphere")
+public class WASModuleListerPlugin implements ModuleListerPlugin {
+    private final RBeanFactory rbf;
+    
+    @Requirement
+    private MBeanServerInspector mbsInspector;
+    
+    @Requirement
+    private Logger log;
+    
+    public WASModuleListerPlugin() {
+        RBeanFactory rbf;
+        try {
+            rbf = new RBeanFactory(AdminServiceFactoryRBean.class);
+        } catch (RBeanFactoryException ex) {
+            rbf = null;
+        }
+        this.rbf = rbf;
+    }
+    
+    public boolean isAvailable() {
+        return rbf != null;
+    }
+    
+    public List<ModuleDescription> listModules() {
+        // TODO: it should be sufficient to do that only once, not for every call to listModules
+        MBeanServer mbs = rbf.createRBean(AdminServiceFactoryRBean.class).getMBeanFactory().getMBeanServer();
+        MBeanRepository repository = mbsInspector.inspect(mbs);
+        if (repository == null) {
+            log.error("Unable to inspect WebSphere's MBean server; this is unexpected because we are in a WebSphere specific plugin");
+            return null;
+        } else {
+            
+//            mbs.queryNames(name, query);
+            // TODO
+            return null;
+        }
+    }
+}
