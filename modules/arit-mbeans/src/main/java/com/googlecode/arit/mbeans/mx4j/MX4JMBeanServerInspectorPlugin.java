12daa13020eb78113d067c9a9a9afb0897bf4656
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-09-19 22:38:56 +0000

    Added support for MX4J, i.e. for Apache Harmony.

---
+++ modules/arit-mbeans/src/main/java/com/googlecode/arit/mbeans/mx4j/MX4JMBeanServerInspectorPlugin.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.mbeans.mx4j;
+
+import javax.management.MBeanServer;
+
+import org.codehaus.plexus.component.annotations.Component;
+
+import com.googlecode.arit.mbeans.MBeanAccessor;
+import com.googlecode.arit.mbeans.MBeanServerInspectorPlugin;
+import com.googlecode.arit.rbeans.RBeanFactory;
+import com.googlecode.arit.rbeans.RBeanFactoryException;
+
+@Component(role=MBeanServerInspectorPlugin.class, hint="mx4j")
+public class MX4JMBeanServerInspectorPlugin implements MBeanServerInspectorPlugin {
+    private final RBeanFactory rbf;
+
+    public MX4JMBeanServerInspectorPlugin() {
+        RBeanFactory rbf;
+        try {
+            rbf = new RBeanFactory(MX4JMBeanServerRBean.class);
+        } catch (RBeanFactoryException ex) {
+            rbf = null;
+        }
+        this.rbf = rbf;
+    }
+    
+    public boolean isAvailable() {
+        return rbf != null;
+    }
+
+    public MBeanAccessor inspect(MBeanServer mbs) {
+        if (rbf.getRBeanInfo(MX4JMBeanServerRBean.class).getTargetClass().isInstance(mbs)) {
+            return new MX4JMBeanAccessor(rbf.createRBean(MX4JMBeanServerRBean.class, mbs).getMBeanRepository());
+        } else {
+            return null;
+        }
+    }
+}
