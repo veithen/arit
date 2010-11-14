2f45aefa86fc54611be3298de85a23c1cddc7924
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2010-11-14 14:20:08 +0000

    We are now able to invoke the ReportGenerator MBean in the systests.

--- systest/arit-systest-tomcat/src/test/java/Test.java
+++ systest/arit-systest-tomcat/src/test/java/Test.java
@@ -1,10 +1,16 @@
 import java.io.File;
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.Engine;
 import org.apache.catalina.Host;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Embedded;
+
+import com.googlecode.arit.report.Report;
 
 public class Test {
 
@@ -28,9 +34,9 @@ public class Test {
 //        container.setAwait(true);
         container.start();
         
-//        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-//        ObjectName reportGeneratorName = (ObjectName)mbs.queryNames(new ObjectName("com.googlecode.arit:type=ReportGenerator"), null).iterator().next();
-//        Report report = (Report)mbs.invoke(reportGeneratorName, "generateReport", new Object[0], new String[0]);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName reportGeneratorName = (ObjectName)mbs.queryNames(new ObjectName("com.googlecode.arit:type=ReportGenerator"), null).iterator().next();
+        Report report = (Report)mbs.invoke(reportGeneratorName, "generateReport", new Object[0], new String[0]);
         
         Object object = new Object();
         synchronized (object) {
