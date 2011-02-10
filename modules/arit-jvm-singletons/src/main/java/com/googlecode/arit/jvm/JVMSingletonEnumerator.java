d9cad473686f46ef07f53807dac9fc6f34cfd224
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-02-10 16:57:57 +0000

    Added support for JVM singletons such as the configured SecurityManager.

---
+++ modules/arit-jvm-singletons/src/main/java/com/googlecode/arit/jvm/JVMSingletonEnumerator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010-2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.jvm;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import com.googlecode.arit.ResourceEnumerator;
+import com.googlecode.arit.ResourceType;
+
+public class JVMSingletonEnumerator implements ResourceEnumerator {
+    private final ResourceType resourceType;
+    private final Iterator<JVMSingleton> iterator;
+    private JVMSingleton singleton;
+    private Object instance;
+    
+    public JVMSingletonEnumerator(ResourceType resourceType, List<JVMSingleton> singletons) {
+        this.resourceType = resourceType;
+        iterator = singletons.iterator();
+    }
+
+    public ResourceType getType() {
+        return resourceType;
+    }
+
+    public Collection<ClassLoader> getClassLoaders() {
+        return Collections.singleton(instance.getClass().getClassLoader());
+    }
+
+    public String getDescription() {
+        return singleton.getDescription() + ": " + instance.getClass().getName();
+    }
+
+    public boolean next() {
+        while (iterator.hasNext()) {
+            singleton = iterator.next();
+            instance = singleton.getInstance();
+            if (instance != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
