11b432210ef8ddc6b0c3db531294f3ccce53256c
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-03-21 11:17:30 +0000

    Added detection for WebSphere bug PM18729.

---
+++ modules/arit-websphere/src/main/java/com/googlecode/arit/websphere/bug/PM18729ResourceEnumeratorFactory.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.websphere.bug;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+
+import com.googlecode.arit.ResourceEnumerator;
+import com.googlecode.arit.ResourceEnumeratorFactory;
+import com.googlecode.arit.ResourceType;
+import com.googlecode.arit.rbeans.RBeanFactory;
+import com.googlecode.arit.rbeans.RBeanFactoryException;
+
+@Component(role=ResourceEnumeratorFactory.class, hint="PM18729")
+public class PM18729ResourceEnumeratorFactory implements ResourceEnumeratorFactory {
+    private final EnvironmentCacheRBean rbean;
+    
+    @Requirement(hint="websphere-bug")
+    private ResourceType resourceType;
+    
+    public PM18729ResourceEnumeratorFactory() {
+        EnvironmentCacheRBean rbean;
+        try {
+            rbean = new RBeanFactory(EnvironmentCacheRBean.class).createRBean(EnvironmentCacheRBean.class);
+        } catch (RBeanFactoryException ex) {
+            rbean = null;
+        }
+        this.rbean = rbean;
+    }
+
+    public String getDescription() {
+        return "EnvironmentCache#root_cache entries (PM18729)";
+    }
+
+    public boolean isAvailable() {
+        return rbean != null;
+    }
+
+    public ResourceEnumerator createEnumerator() {
+        return new PerClassLoaderCacheResourceEnumerator(resourceType, "PM18729: org.apache.commons.discovery.tools.EnvironmentCache#root_cache entry", rbean.getRootCache());
+    }
+}
