113ee64a3887bcbd2798dd11b6daf54c414a09a8
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-05-26 21:47:42 +0000

    Some very early Geronimo code.

---
+++ modules/arit-geronimo/src/main/java/com/googlecode/arit/geronimo/GeronimoModuleInspector.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010-2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.geronimo;
+
+import java.util.List;
+
+import com.googlecode.arit.ModuleDescription;
+import com.googlecode.arit.ModuleInspector;
+import com.googlecode.arit.ModuleStatus;
+import com.googlecode.arit.ModuleType;
+import com.googlecode.arit.rbeans.RBeanFactory;
+
+public class GeronimoModuleInspector implements ModuleInspector {
+    private final RBeanFactory rbf;
+    private final ModuleType warModuleType;
+
+    public GeronimoModuleInspector(RBeanFactory rbf, ModuleType warModuleType) {
+        this.rbf = rbf;
+        this.warModuleType = warModuleType;
+    }
+
+    public List<ModuleDescription> listModules() {
+        return null;
+    }
+
+    public ModuleDescription inspect(ClassLoader classLoader) {
+        if (rbf.getRBeanInfo(JarFileClassLoaderRBean.class).getTargetClass().isInstance(classLoader)) {
+            JarFileClassLoaderRBean jfcl = rbf.createRBean(JarFileClassLoaderRBean.class, classLoader);
+            // TODO: obviously, not all modules are WARs
+            // TODO: we should be able to extract the URL as well
+            return new ModuleDescription(warModuleType, jfcl.getId().toString(), classLoader, null,
+                    jfcl.isDestroyed() ? ModuleStatus.STOPPED : ModuleStatus.STARTED);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ modules/arit-geronimo/src/main/java/com/googlecode/arit/geronimo/GeronimoModuleInspectorPlugin.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010-2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.geronimo;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+
+import com.googlecode.arit.ModuleInspector;
+import com.googlecode.arit.ModuleInspectorPlugin;
+import com.googlecode.arit.ModuleType;
+import com.googlecode.arit.rbeans.RBeanFactory;
+import com.googlecode.arit.rbeans.RBeanFactoryException;
+
+@Component(role=ModuleInspectorPlugin.class, hint="geronimo")
+public class GeronimoModuleInspectorPlugin implements ModuleInspectorPlugin {
+    private final RBeanFactory rbf;
+    
+    @Requirement(hint="war")
+    private ModuleType warModuleType;
+    
+    public GeronimoModuleInspectorPlugin() {
+        RBeanFactory rbf;
+        try {
+            rbf = new RBeanFactory(JarFileClassLoaderRBean.class);
+        } catch (RBeanFactoryException ex) {
+            rbf = null;
+        }
+        this.rbf = rbf;
+    }
+    
+    public boolean isAvailable() {
+        return rbf != null;
+    }
+
+    public ModuleInspector createModuleInspector() {
+        return new GeronimoModuleInspector(rbf, warModuleType);
+    }
+}
