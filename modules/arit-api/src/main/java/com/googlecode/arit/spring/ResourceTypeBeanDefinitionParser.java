c5fd576f5d3bcf3b3302c2f9207e2785dd026db8
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-08-07 20:25:39 +0000

    Use Spring bean definition parser to simplify resource type definition.

---
+++ modules/arit-api/src/main/java/com/googlecode/arit/spring/ResourceTypeBeanDefinitionParser.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.arit.spring;
+
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.AutowireCandidateQualifier;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+import com.googlecode.arit.ResourceTypeFactory;
+
+public class ResourceTypeBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+    @Override
+    protected Class<?> getBeanClass(Element element) {
+        return ResourceTypeFactory.class;
+    }
+
+    @Override
+    protected void postProcess(BeanDefinitionBuilder builder, Element element) {
+        AbstractBeanDefinition definition = builder.getRawBeanDefinition();
+        definition.addQualifier(new AutowireCandidateQualifier(Qualifier.class, definition.getPropertyValues().getPropertyValue("identifier").getValue()));
+    }
+
+    @Override
+    protected String resolveId(Element element, AbstractBeanDefinition definition, ParserContext parserContext) throws BeanDefinitionStoreException {
+        return "resource-type:" + definition.getPropertyValues().getPropertyValue("identifier").getValue();
+    }
+}
