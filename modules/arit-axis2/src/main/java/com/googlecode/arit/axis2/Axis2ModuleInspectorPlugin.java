9b4e9b67fac3d56f5a532b0fea9b9aca1a6c0ebd
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-08-08 18:08:55 +0000

    Improved identification of Axis2 MAR and AAR components.

--- modules/arit-axis2/src/main/java/com/googlecode/arit/axis2/Axis2ModuleInspectorPlugin.java
+++ modules/arit-axis2/src/main/java/com/googlecode/arit/axis2/Axis2ModuleInspectorPlugin.java
@@ -18,6 +18,8 @@ package com.googlecode.arit.axis2;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -29,6 +31,8 @@ import com.googlecode.arit.ModuleStatus;
 import com.googlecode.arit.ModuleType;
 
 public class Axis2ModuleInspectorPlugin implements ModuleInspectorPlugin {
+    static final Pattern tempArchivePattern = Pattern.compile("axis2[0-9]+(.*)");
+    
     @Autowired
     @Qualifier("mar")
     ModuleType marModuleType;
@@ -52,8 +56,33 @@ public class Axis2ModuleInspectorPlugin implements ModuleInspectorPlugin {
                     URLClassLoader cl = (URLClassLoader)classLoader;
                     String clClassName = cl.getClass().getName();
                     if (clClassName.equals("org.apache.axis2.deployment.DeploymentClassLoader") || clClassName.equals("org.apache.axis2.classloader.JarFileClassLoader")) {
+                        // Note: While a MAR always contains a module.xml file, Axis2 allows to deploy services without
+                        //       a services.xml file. However, Axis2 creates a multi-level class loader hierarchy and
+                        //       all these class loaders are of type DeploymentClassLoader or JarFileClassLoader. If we
+                        //       don't find a module.xml or services.xml file, it is impossible to tell if the class
+                        //       loader belongs to a service or if it is an intermediary class loader. Therefore we only
+                        //       report class loaders as Axis2 services if we find a services.xml file.
+                        ModuleType moduleType;
+                        if (cl.findResource("META-INF/module.xml") != null) {
+                            moduleType = marModuleType;
+                        } else if (cl.findResource("META-INF/services.xml") != null) {
+                            moduleType = aarModuleType;
+                        } else {
+                            return null;
+                        }
+                        // Axis2 always adds the URL of the MAR or AAR as the first entry in the classpath. However,
+                        // it will also copy the archive to a temporary directory and rename it. A typical file name
+                        // looks as follows:
+                        //   axis26493945763371868088scripting-1.6.0.mar
+                        // We do an attempt here to reconstruct the original file name.
                         URL url = cl.getURLs()[0];
-                        return new ModuleDescription(cl.findResource("META-INF/module.xml") == null ? aarModuleType : marModuleType, url.getFile(), classLoader, url, ModuleStatus.UNKNOWN);
+                        String archiveName = url.getFile();
+                        archiveName = archiveName.substring(archiveName.lastIndexOf('/')+1);
+                        Matcher matcher = tempArchivePattern.matcher(archiveName);
+                        if (matcher.matches()) {
+                            archiveName = matcher.group(1);
+                        }
+                        return new ModuleDescription(moduleType, archiveName, classLoader, url, ModuleStatus.UNKNOWN);
                     } else {
                         return null;
                     }
