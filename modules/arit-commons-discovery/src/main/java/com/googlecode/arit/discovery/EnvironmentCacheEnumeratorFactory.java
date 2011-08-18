632a5c9b99c5d97beaf4c936495378d42b306d93
andreas.veithen <andreas.veithen@868386ec-2aea-11df-8aff-a9a8dab06da4>
2011-08-18 08:04:52 +0000

    Added some unfinished code for commons-discovery.

---
+++ modules/arit-commons-discovery/src/main/java/com/googlecode/arit/discovery/EnvironmentCacheEnumeratorFactory.java
@@ -0,0 +1,24 @@
+package com.googlecode.arit.discovery;
+
+import com.googlecode.arit.ResourceEnumerator;
+import com.googlecode.arit.ResourceEnumeratorFactory;
+
+public class EnvironmentCacheEnumeratorFactory implements ResourceEnumeratorFactory<ResourceEnumerator> {
+
+    public ResourceEnumerator createEnumerator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean isAvailable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+
+}
